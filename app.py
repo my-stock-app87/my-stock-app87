364bd83c59feaf6129daa4ca50cf33aff6d202c9
my-stock-app87 <01047261489@hanmail.net>
2026-05-31 06:57:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from scanner.run_scan import run_ai_scan
 # =========================
 # 기본 설정
 # =========================
+
+
 st.set_page_config(
     page_title="주식주신 PRO Mobile",
     layout="centered"
